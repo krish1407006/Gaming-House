00a2901cad974ec1f06a117ca44c96539a8f3ee4
krish1407006 <krish14072006@gmail.com>
2026-06-26 19:48:22 +0530

    gh

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -30,7 +30,7 @@ function GameCard({ game }) {
             }
           }}
         />
-        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-500 pointer-events-none z-10"></div>
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-20 transition-opacity duration-500 pointer-events-none z-10"></div>
       </Link>
 
       <div className="p-2 sm:p-3 lg:p-4 flex flex-col flex-grow min-h-0">
